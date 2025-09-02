93e6ba9b80610fd2ae1b6852c986a734f4c9287e
job4j_elementary <aleksei-korotkov@yandex.ru>
2025-09-03 01:12:01 +0300

    4.2. Интерфейс Input [#181092 #551227]

--- src/main/java/ru/job4j/polymorphism/Bus.java
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -1,18 +1,21 @@
 package ru.job4j.polymorphism;
 
 public class Bus implements Transport {
+    private int passengersCount;
+    private int fuelCost;
+
     @Override
     public void move() {
-
+        System.out.println("Выполняется метод Bus.move()");
     }
 
     @Override
     public void passengers(int count) {
-
+        passengersCount = count;
     }
 
     @Override
     public int refuel(int fuel) {
-        return 0;
+        return fuelCost * fuel;
     }
 }
--- src/main/java/ru/job4j/polymorphism/Transport.java
+++ src/main/java/ru/job4j/polymorphism/Transport.java
@@ -1,6 +1,7 @@
 package ru.job4j.polymorphism;
 
 public interface Transport {
+
     void move();
 
     void passengers(int count);
